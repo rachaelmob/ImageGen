041a8bda458fe27f1e86954a2edb08504530ba8f
rachaelmob <rachael.mo.b@gmail.com>
2024-05-01 23:51:28 +0100

    nothing

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let $genImg = $('.gen-img');
+const $genBtn = $('.gen-btn');
+const $getEmail = $('#user_email');
+const $submitEmail = $('#submit_email');
+
+$(document).ready(function() {
+
+
+});
+
+
+// Fetch Picsum  Function And Apply Img to SRC
+function fetchRandomPic() {
+ fetch('https://picsum.photos/200/300')
+ .then(response => response.json())
+
+
+
+
+
+}
+
+// Get Email from user 
+
+// Validate Email 
+
+// Store Email locally inside drop menu 
