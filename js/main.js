013c5e95ff7e5025ba8ae84d3965402fe00ab095
rachaelmob <rachael.mo.b@gmail.com>
2024-05-02 10:43:20 +0100

    js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,16 @@ $(document).ready(function() {
 // Fetch Picsum  Function And Apply Img to SRC
 function fetchRandomPic() {
  fetch('https://picsum.photos/200/300')
- .then(response => response.json())
+    .then(response => {
+        if (response.ok) {
+            console.log('FETCH ACCEPTED')
+        } else {
+            console.log('FETCH DENIED')
+        }
+        response.json()
+    })
+    .then(data => console.log(data))
+
 
 
 
